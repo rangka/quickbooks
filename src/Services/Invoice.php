f84b5ac85e3434e8c23402bad16a4b112cd2f941
Khairul Ashraff <khairul@ashraff.my>
2017-03-14 21:10:20 +0800

    Added send method to Invoice.

--- src/Services/Invoice.php
+++ src/Services/Invoice.php
@@ -13,11 +13,32 @@ class Invoice extends Service {
      * Download Invoice as PDF
      *
      * @param string $id Invoice ID.
+     * 
      * @return \GuzzleHttp\Psr7\Stream
      */
     public function downloadPdf($id) {
         return $this->request('GET', $this->getResourceName() . '/' . $id . '/pdf', [], [
             'Accept' => 'application/pdf'
+        ]);
+    }
+
+    /**
+     * Send an invoice through email.
+     *
+     * @param string $id    Invoice ID.
+     * @param string $email Email to be sent to.
+     * 
+     * @return \GuzzleHttp\Psr7\Stream
+     */
+    public function send($id, $email = null) {
+        $url = $this->getResourceName() . '/' . $id . '/send';
+
+        if ($email) {
+            $url .= '?sendTo=' . urlencode($email);
+        }
+
+        return $this->request('POST', $url, [], [
+            'Content-Type' => 'application/octet-stream'
         ]);
     }
 }
