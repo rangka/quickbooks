69590229fd447be7def73bf6264ce4d3b68d722e
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 16:44:52 +0800

    Add Attachable to Bill.

--- src/Services/Bill.php
+++ src/Services/Bill.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class Bill extends Service {
-    
+    use Attachable;
 }
