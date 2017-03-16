8bdf4ca2a2327999ee73a302d070a5079b0729c9
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 17:00:21 +0800

    Add Attachable to Estimate.

--- src/Services/Estimate.php
+++ src/Services/Estimate.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class Estimate extends Service {
-    
+    use Attachable;
 }
