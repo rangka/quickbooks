45e00104d3a033f439f026900fc65ad47cdceacf
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 16:40:40 +0800

    Add Attachable to Purchase.

--- src/Services/Purchase.php
+++ src/Services/Purchase.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class Purchase extends Service {
-    
+    use Attachable;
 }
