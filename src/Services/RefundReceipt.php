de9eac1d1e6029566ef94e5e3a4dbb7b8693bf4c
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 16:57:55 +0800

    Add Attachable to RefundReceipt.

--- src/Services/RefundReceipt.php
+++ src/Services/RefundReceipt.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class RefundReceipt extends Service {
-    
+    use Attachable;
 }
