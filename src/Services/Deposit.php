0c16b7c77182e95f9d480630d1a142e84e6bc165
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 17:11:20 +0800

    Add Attachable to Deposit.

--- src/Services/Deposit.php
+++ src/Services/Deposit.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class Deposit extends Service {
-    
+    use Attachable;
 }
