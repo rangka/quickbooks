2b569f9b5aa83a4fffac2dbfdad48efe72785948
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 16:28:25 +0800

    Added Attachable to Customer.

--- src/Services/Customer.php
+++ src/Services/Customer.php
@@ -4,8 +4,11 @@ namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Builders\Address;
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class Customer extends Service {
+	use Attachable;
+
     /**
     * Get an instance of Address Builder to build Adrdress
     * @return \Rangka\Quickbooks\Builders\Address
