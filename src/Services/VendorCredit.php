cab9e3d27a4b01fd30a9ffae0765e6673af80cdd
Khairul Ashraff <khairul@ashraff.my>
2017-03-16 17:26:30 +0800

    Add Attachable to VendorCredit.

--- src/Services/VendorCredit.php
+++ src/Services/VendorCredit.php
@@ -3,7 +3,8 @@
 namespace Rangka\Quickbooks\Services;
 
 use Rangka\Quickbooks\Client;
+use Rangka\Quickbooks\Services\Traits\Attachable;
 
 class VendorCredit extends Service {
-    
+    use Attachable;
 }
