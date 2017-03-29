d54da117c318c8b95fc67db8151cc4dbad60bd9e
Jens Trio <jens@jnstr.be>
2017-03-29 10:26:39 +0200

    Fix load-function for Preferences service
    According to the Quickbooks documentation, the URI for the 
    Preferences detail is "/company/:companyId/preferences".
    The default load function generated the URI in the format
    "/company/:companyId/preferences/:companyId", resulting in an API
    error.
    This commit fixes this.

--- src/Services/Preferences.php
+++ src/Services/Preferences.php
@@ -5,4 +5,14 @@ namespace Rangka\Quickbooks\Services;
 use Rangka\Quickbooks\Client;
 
 class Preferences extends Service {
+  
+  /**
+     * Load a single item
+     *
+     * @return
+     */
+    public function load($id) {
+        return parent::get($this->getResourceName())->{$this->getEntityName()};
+    }
+  
 }
