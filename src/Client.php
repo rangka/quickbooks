419a3f4204590bcad92c4135b910f1f416a737d5
Khairul Ashraff <khairul@ashraff.my>
2019-04-15 16:05:04 +0800

    Migrate to OAuth2 for all API requests.

--- src/Client.php
+++ src/Client.php
@@ -78,84 +78,14 @@ class Client {
     }
 
     /**
-    * Sign a request
-    * 
-    * @param string     $url        Endpoint
-    * @param array      $params     Array of parameters to be signed.
-    * @param string     $secret     Token secret to be appended to consumer secret for signing.
-    * @return array                 string - base string to be signed, 
-    *                               url    - signed URL (null if QuickBooks has been connected)
-    *                               header - Authorization header (null if QuickBooks has not been connected)
-    */
-    protected function sign($method, $url, $params = []) {
-        // parse URL
-        $parsedURL = parse_url($url);
-
-        // reconstruct it with only what we need
-        $url = $parsedURL['scheme'] . '://' . $parsedURL['host'] . $parsedURL['path'];
-
-        // set default parameters and sort it by key
-        $params = array_merge([
-            'oauth_consumer_key'     => self::$consumer_key,
-            'oauth_nonce'            => substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10),
-            'oauth_signature_method' => 'HMAC-SHA1',
-            'oauth_timestamp'        => time(),
-            'oauth_version'          => '1.0'
-        ], $params);
-
-        // set query parameters if exists
-        if (isset($parsedURL['query'])) {
-            $parsedQuery = [];
-            parse_str($parsedURL['query'], $parsedQuery);
-            $params = array_merge($parsedQuery, $params);
-        }
-
-        // sort parameters by key
-        ksort($params);
-
-        // generate string to be signed
-        $string = $method . '&' . rawurlencode($url) . '&' . rawurlencode(http_build_query($params, null, '&', PHP_QUERY_RFC3986));
-
-        // calculate signature
-        $params['oauth_signature'] = base64_encode(hash_hmac('sha1', $string, rawurlencode(self::$consumer_secret) . '&' . rawurlencode(self::$oauth_token_secret), true));
-
-        // generate auth header if oauth_token is present but without verifier (actual API request and not authorization process)
-        $header = $signed_url = '';
-        if (isset($params['oauth_token']) && !isset($params['oauth_verifier'])) {
-            $header = 'OAuth ' . implode(', ', [
-                'oauth_signature_method="'.$params['oauth_signature_method'].'"',
-                'oauth_signature="'.rawurlencode($params['oauth_signature']).'"',
-                'oauth_nonce="'.$params['oauth_nonce'].'"',
-                'oauth_timestamp="'.$params['oauth_timestamp'].'"',
-                'oauth_token="'.$params['oauth_token'].'"',
-                'oauth_consumer_key="'.$params['oauth_consumer_key'].'"',
-                'oauth_version="'.$params['oauth_version'].'"'
-            ]);
-        } else {
-            // build URL for signed URL request
-            $signed_url = $url . '?' . http_build_query($params);
-        }
-
-        // return all results
-        return [
-            'string' => $string,
-            'url'    => $signed_url,
-            'header' => $header
-        ];
-    }
-
-    /**
     * Request from Quickbooks.
     * 
     * @return 
     */
     public function request($method, $url, $body = [], $headers = []) {
         $url      = trim($url, '/');
-        $base_uri = $this->getBaseURL() . '/' . self::$company_id . '/';
+        $base_uri = $this->getBaseURL() . '/' . self::$realm_id . '/';
         $full_url = $base_uri . $url;
-        $signed   = $this->sign($method, $full_url, [
-            'oauth_token' => self::$oauth_token
-        ]);
 
         $headers  = array_merge([
             'Accept'       => 'application/json',
@@ -167,7 +97,7 @@ class Client {
             'headers'  => [
                 'Accept'        => $headers['Accept'],
                 'Content-Type'  => $headers['Content-Type'],
-                'Authorization' => $signed['header']
+                'Authorization' => 'Bearer ' . self::$oauth['access_token'],
             ],
         ];
 
