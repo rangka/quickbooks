3d5536208b55356c18ad34859b64247ad0cef0f5
Khairul Ashraff <khairul@ashraff.my>
2019-04-16 15:12:51 +0800

    Formatting.

--- src/Webhook.php
+++ src/Webhook.php
@@ -3,22 +3,22 @@
 namespace Rangka\Quickbooks;
 
 class Webhook extends Client {
-	/**
-	 * Validate an incoming webhook.
-	 *
-	 * @return boolean
-	 */
-	public function validate()
-	{
-	    $inputSignature = $_SERVER['HTTP_INTUIT_SIGNATURE'] ?? null;
+    /**
+     * Validate an incoming webhook.
+     *
+     * @return boolean
+     */
+    public function validate()
+    {
+        $inputSignature = $_SERVER['HTTP_INTUIT_SIGNATURE'] ?? null;
 
-	    if (!$inputSignature) {
-	    	return false;
-	    }
+        if (!$inputSignature) {
+            return false;
+        }
 
-		$body = file_get_contents('php://input');
-	    $calculatedSignature = base64_encode(hash_hmac('sha256', $body, self::$webhook_token, true));
+        $body = file_get_contents('php://input');
+        $calculatedSignature = base64_encode(hash_hmac('sha256', $body, self::$webhook_token, true));
 
-	    return $inputSignature == $calculatedSignature;
-	}
+        return $inputSignature == $calculatedSignature;
+    }
 }
